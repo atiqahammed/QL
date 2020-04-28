f6f2e18de0b53ce58896d49b4c317379d05a4b52
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-04-29 01:57:32 +0600

    foreign key

---
+++ basic/Table/0001_Create/ForeignKey/CREATE_TABLE_0001.sql
@@ -0,0 +1,17 @@
+USE [Test1]
+
+CREATE TABLE [Product]
+(
+	product_id INT PRIMARY KEY,
+	product_name VARCHAR(50) NOT NULL,
+	product_categoty VARCHAR(50)
+);
+
+CREATE TABLE [Order]
+(
+	order_id INT PRIMARY KEY,
+	product_id INT NOT NULL,
+	CONSTRAINT product_foreign_key 
+		FOREIGN KEY (product_id)
+		REFERENCES [Product] (product_id) 
+);
