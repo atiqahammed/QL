1ab3f53e03e864d4c8e73b3a9f423f76ad5c759b
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-04-29 03:02:35 +0600

    Create a Database Schema

---
+++ basic/Create a Database Schema/SQLQuery8.sql
@@ -0,0 +1,7 @@
+CREATE SCHEMA Chains;
+GO
+CREATE TABLE Chains.Sizes (ChainID int, width dec(10,2));
+
+SELECT * FROM sys.schemas;
+
+/* https://docs.microsoft.com/en-us/sql/relational-databases/security/authentication-access/create-a-database-schema?view=sql-server-ver15 */ 
